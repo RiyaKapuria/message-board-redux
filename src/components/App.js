3da73058e9e67180035a318bfd39982d4632a6eb
RiyaKapuria <riyakapuria.1992@gmail.com>
2018-12-25 15:58:05 +0530

    add redux structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+
 import "bootstrap/dist/css/bootstrap.css";
 import Navbar from "./Navbar";
 import PublicBoard from "./publicBoard/PublicBoard";
 import PrivateMessage from "./privateBoard/PrivateBoard";
+import { postReducer } from "../reducers/post_reducer";
+
+const rootReducer = combineReducers({
+  posts: postReducer
+});
+const store = createStore(rootReducer);
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <Provider store={store}>
         <BrowserRouter>
           <div>
             <Navbar />
@@ -16,7 +25,7 @@ class App extends React.Component {
             <Route path="/private" component={PrivateMessage} />
           </div>
         </BrowserRouter>
-      </div>
+      </Provider>
     );
   }
 }
--- src/components/publicBoard/PublicForm.js
+++ src/components/publicBoard/PublicForm.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../../actions";
 
 class PublicForm extends Component {
   state = {
@@ -35,4 +37,7 @@ class PublicForm extends Component {
   }
 }
 
-export default PublicForm;
+export default connect(
+  null,
+  actions
+)(PublicForm);
